37f49c361db0d4735f0afa18fb36e3d13a181c6f
sumitd-555 <aws.cloud2603@gmail.com>
2026-04-28 15:51:43 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "frontend_server" {
   ami                    = "ami-0f58b397bc5c1f2e8"
-  instance_type          = "t2.micro"
+  instance_type          = "t3.small"
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
   key_name               = "your-keypair-name"
