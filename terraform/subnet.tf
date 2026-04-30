5b7bb75a44a91c0ee3c7c674de05c02269bffb3a
sumitd-555 <aws.cloud2603@gmail.com>
2026-05-01 00:00:16 +0530

    Update subnet.tf

--- terraform/subnet.tf
+++ terraform/subnet.tf
@@ -9,6 +9,25 @@ resource "aws_subnet" "public_subnet" {
   }
 }
 
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+
 resource "aws_subnet" "private_subnet_1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
