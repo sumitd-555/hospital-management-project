6bd774795274ff8c0e779d60df9513755be3cdd4
sumitd-555 <aws.cloud2603@gmail.com>
2026-04-30 23:19:01 +0530

    Update rds.tf

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,40 +1,3 @@
-# subnet + rds combined file
-
-# Public Subnet
-resource "aws_subnet" "public_subnet" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "ap-south-1a"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "public-subnet"
-  }
-}
-
-# Private Subnet 1
-resource "aws_subnet" "private_subnet_1" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "ap-south-1a"
-
-  tags = {
-    Name = "private-subnet-1"
-  }
-}
-
-# Private Subnet 2
-resource "aws_subnet" "private_subnet_2" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.3.0/24"
-  availability_zone = "ap-south-1b"
-
-  tags = {
-    Name = "private-subnet-2"
-  }
-}
-
-# RDS DB Subnet Group
 resource "aws_db_subnet_group" "default" {
   name = "hospital-db-subnet-group"
 
@@ -48,7 +11,6 @@ resource "aws_db_subnet_group" "default" {
   }
 }
 
-# RDS MySQL Database
 resource "aws_db_instance" "mysql_db" {
   allocated_storage    = 20
   engine               = "mysql"
