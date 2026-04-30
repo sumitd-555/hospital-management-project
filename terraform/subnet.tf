5ac06846469624d276e534e01b4990c71891107b
sumitd-555 <aws.cloud2603@gmail.com>
2026-04-30 23:18:17 +0530

    Update subnet.tf

--- terraform/subnet.tf
+++ terraform/subnet.tf
@@ -1,20 +1,19 @@
-resource "aws_subnet" "public_subnet" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "ap-south-1a"
-  map_public_ip_on_launch = true
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-south-1a"
 
   tags = {
-    Name = "public-subnet"
+    Name = "private-subnet-1"
   }
 }
 
-resource "aws_subnet" "private_subnet" {
+resource "aws_subnet" "private_subnet_2" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.0/24"
+  cidr_block        = "10.0.3.0/24"
   availability_zone = "ap-south-1b"
 
   tags = {
-    Name = "private-subnet"
+    Name = "private-subnet-2"
   }
 }
